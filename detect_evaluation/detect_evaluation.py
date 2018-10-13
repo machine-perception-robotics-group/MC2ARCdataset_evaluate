955a903f3c732c85737c539bf5adac7bd6f974e2
Ryosuke Araki <ryorsk@mprg.cs.chubu.ac.jp>
2018-10-13 16:45:11 +0900

    update & modify
    - detect_evaluation.py: move some function to myfunc.py
    - consistency_evaluation.py: it evaluate consistency between detect and segmentation.

--- detect_evaluation/detect_evaluation.py
+++ detect_evaluation/detect_evaluation.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from pylab import *
 import argparse
 
+import myfunc
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--image', '-i', type = str, default = './image/', help = 'Path of test images dir')
 parser.add_argument('--teach', '-t', type = str, default = './teach/', help = 'Path of ground truth b-boxes dir')
@@ -32,7 +34,7 @@ IMAGE_EXT = "png"
 WAITTIME = 0
 
 # if your detection results have a classlabel(e.g.: DVD, avery_binder...), set 1
-LABEL_FLAG = 0
+LABEL_FLAG = 1
 
 # if your detection results are normalized, set 1
 NORMALIZED = 0
@@ -50,197 +52,29 @@ NCLASS = 40
 
 ##################################################
 
+COLOR_TABLE = myfunc.COLOR_TABLE
 
-
-COLOR_TABLE = [
-[   0,    0,    0],
-[  85,    0,    0],
-[ 170,    0,    0],
-[ 255,    0,    0],
-[   0,   85,    0],
-[  85,   85,    0],
-[ 170,   85,    0],
-[ 255,   85,    0],
-[   0,  170,    0],
-[  85,  170,    0],
-[ 170,  170,    0],
-[ 255,  170,    0],
-[   0,  255,    0],
-[  85,  255,    0],
-[ 170,  255,    0],
-[ 255,  255,    0],
-[   0,    0,   85],
-[  85,    0,   85],
-[ 170,    0,   85],
-[ 255,    0,   85],
-[   0,   85,   85],
-[  85,   85,   85],
-[ 170,   85,   85],
-[ 255,   85,   85],
-[   0,  170,   85],
-[  85,  170,   85],
-[ 170,  170,   85],
-[ 255,  170,   85],
-[   0,  255,   85],
-[  85,  255,   85],
-[ 170,  225,   85],
-[ 255,  255,   85],
-[   0,    0,  170],
-[  85,    0,  170],
-[ 170,    0,  170],
-[ 255,    0,  170],
-[   0,   85,  170],
-[  85,   85,  170],
-[ 170,   85,  170],
-[ 255,   85,  170],
-[   0,  170,  170],
-[ 255,  255,  255]]
-
-itemIDList = [
-"0 BG",
-"1",
-"2",
-"3",
-"4",
-"5",
-"6",
-"7",
-"8",
-"9",
-"10",
-"11",
-"12",
-"13",
-"14",
-"15",
-"16",
-"17",
-"18",
-"19",
-"20",
-"21",
-"22",
-"23",
-"24",
-"25",
-"26",
-"27",
-"28",
-"29",
-"30",
-"31",
-"32",
-"33",
-"34",
-"35",
-"36",
-"37",
-"38",
-"39",
-"40",
-"41"]
-
-def convNormalizedCord(data, height, width):
-    x = float(data[1]) * width
-    y = float(data[2]) * height
-    w = float(data[3]) * width
-    h = float(data[4]) * height
-    x1 = x - (w/2.)
-    y1 = y - (h/2.)
-    x2 = x + (w/2.)
-    y2 = y + (h/2.)
-    return(int(data[0]), int(x1), int(y1), int(x2), int(y2))
-
-def convResCord(data, height, width):
-    if NORMALIZED == 1:
-        data = convNormalizedCord(data, height, width)
-    if int(data[0]) == 0:
-        classID = 41
-    else:
-        classID = int(data[0])
-    return(classID, int(data[1]), int(data[2]), int(data[3]), int(data[4]) )
-
-def getIOU(boxA, boxB):
-    # if the length of between boxAcenter and boxBcenter is too far, return 0
-    center_boxA = numpy.array([(boxA[0] + boxA[2]) / 2.0, (boxA[1] + boxA[3]) / 2.0])
-    center_boxB = numpy.array([(boxB[0] + boxB[2]) / 2.0, (boxB[1] + boxB[3]) / 2.0])
-    if numpy.linalg.norm(center_boxA - center_boxB) >= 500:
-        return 0
-
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
-
-    inter_area = (xB - xA + 1) * (yB - yA + 1)
-    boxA_area = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxB_area = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-
-    iou = inter_area / float(boxA_area + boxB_area - inter_area)
-
-    return iou
-
-def readTxt(file_path, d_type):
-    coordinate = []
-    f = open(file_path, 'r')
-
-    if f != None:
-        for row in f:
-            data = row.split()
-            if(LABEL_FLAG==1 and d_type == "result"):
-                data = [data[0], data[2], data[3], data[4], data[5]]
-            elif(d_type == "result"):
-                data = [data[0], data[1], data[2], data[3], data[4]]
-            elif(CAT_PASS_FLAG==1 and d_type == "teach"):
-                data = [data[0], data[3], data[4], data[5], data[6]]
-            elif(d_type == "teach" or d_type == "evaluate"):
-                data = data
-                # DO NOTHING
-            else:
-                print("[ERROR] Unexpected text data type:" + d_type)
-                return 1
-            coordinate.append(data)
-        f.close()
-        return coordinate
-    else:
-        print("[ERROR] Can't read:" + file_path)
-        return 1
-
-def drawBB(img, data):
-    color = [ COLOR_TABLE[int(data[0])][2], COLOR_TABLE[int(data[0])][1], COLOR_TABLE[int(data[0])][0] ]
-    height, width = img.shape[:2]
-    x1 = data[1]
-    y1 = data[2]
-    x2 = data[3]
-    y2 = data[4]
-    cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-    cv2.putText(img, itemIDList[int(data[0])], (x1, y1-2), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 2)
-    cv2.putText(img, itemIDList[int(data[0])], (x1, y1), cv2.FONT_HERSHEY_TRIPLEX, 1, (0,0,0), 1)
-    return img
-
-def drawAllBB(img, coordinate):
-    for data in coordinate:
-        img = drawBB(img, data)
-    return img
+itemIDList = myfunc.itemIDList
 
 def matching(file_list):
     for file_path in file_list:
         print(file_path)
         file_name, ext = path.splitext( path.basename(file_path) )
+        file_name = file_name.replace("res", "")
 
-        result_data = readTxt(file_path, "result")
-        teach_data = readTxt(teach_path + file_name + ".txt", "teach")
+        result_data = myfunc.readTxt(file_path, "result", label_flag=LABEL_FLAG)
+        teach_data = myfunc.readTxt(teach_path + file_name + ".txt", "teach", cat_pass_flag=CAT_PASS_FLAG)
         img = cv2.imread(image_path + file_name + "." + IMAGE_EXT)
         height, width = img.shape[:2]
         img_backup = img.copy()
 
         # Convert to Full(teach) coordinates
         for i in range(0, len(teach_data)):
-            teach_data[i] = convNormalizedCord(teach_data[i], height, width)
+            teach_data[i] = myfunc.convNormalizedCord(teach_data[i], height, width)
 
         # Convert for result coordinates
         for i in range(0, len(result_data)):
-            result_data[i] = convResCord(result_data[i], height, width)
+            result_data[i] = myfunc.convResCord(result_data[i], height, width, NORMALIZED)
 
         # init result lists
         hit = [False] * len(teach_data)
@@ -253,14 +87,14 @@ def matching(file_list):
             max_IoU = 0.0
             max_index = 0
             for i in range(0, len(teach_data)):
-                iou = getIOU(teach_data[i][1:], result_data[j][1:])
+                iou = myfunc.getIOU(teach_data[i][1:], result_data[j][1:])
                 if(max_IoU < iou) and (iou <= 1.0):
                     max_IoU = iou
                     max_index = i
                 if WAITTIME != 0:
                     img = img_backup.copy()
-                    img = drawBB(img, teach_data[i])
-                    img = drawBB(img, result_data[j])
+                    img = myfunc.drawBB(img, teach_data[i])
+                    img = myfunc.drawBB(img, result_data[j])
                     cv2.imshow("", img)
                     cv2.waitKey(WAITTIME)
 
@@ -272,13 +106,13 @@ def matching(file_list):
                 success[j] = (teach_data[max_index][0] == result_data[j][0])
                 true_category[j] = teach_data[max_index][0]
                 if WAITTIME != 0:
-                    img = drawBB(img, teach_data[max_index])
-                    img = drawBB(img, result_data[j])
+                    img = myfunc.drawBB(img, teach_data[max_index])
+                    img = myfunc.drawBB(img, result_data[j])
                     cv2.putText(img, "Max IoU:" + str(max_IoU), (0, 25) , cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 2)
                     cv2.putText(img, "Class Match:" + str(success[j]), (0, 50) , cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 2)
             else:
                 if WAITTIME != 0:
-                    img = drawBB(img, result_data[j])
+                    img = myfunc.drawBB(img, result_data[j])
                     cv2.putText(img, "No Matching", (0, 25) , cv2.FONT_HERSHEY_TRIPLEX, 1, (255,255,255), 2)
             if WAITTIME != 0:
                 cv2.imshow("", img)
@@ -343,7 +177,7 @@ def evaluate(file_list):
         false_boxes = 0
         undetected_boxes = 0
 
-        result_data = readTxt(filePath, "evaluate")
+        result_data = myfunc.readTxt(filePath, "evaluate")
 
         for i in range(0, len(result_data)):
             boxes += 1
